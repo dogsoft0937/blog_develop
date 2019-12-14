279dca5449085553087c1a3a70a15fb826ea1108
dogsoft0937 <dabin0450@naver.com>
2019-12-14 23:23:14 +0900

    no modularization&implement passport system

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var app=express()
 var bodyParser = require('body-parser');
 var path = require('path');
 var http=require("http").createServer(app),port=3000
-var register=require("./routes/register")
-var login=require("./routes/login")
+var auth=require("./routes/auth")
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'./views'));
@@ -12,12 +11,11 @@ app.set('views',path.join(__dirname,'./views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(__dirname + "/views"));
-app.use("/register",register)
-app.use("/login",login)
-
+app.use("/auth",auth)
 app.get("/",function(req,res){
   // res.sendFile(path.join(__dirname,"/views/index.html"))
-  res.render(path.join(__dirname,"/views/index"))
+  // res.render(path.join(__dirname,"/views/index"),{"message":req.flash()})
+  res.redirect('/auth/login')
 })
 http.listen(port,function(){
   console.log("http://localhost:3000")
